2cc59818886dc7c2c0515d39798d1a6763caafa9
Swaathi Sundaramurugan <ssa388@sfu.ca>
2022-03-11 18:11:11 -0800

    missing NFT code

--- getMissingNFTs.py
+++ getMissingNFTs.py
@@ -1,3 +1,5 @@
+import requests
+import time
 import pandas as pd
 import json
 
@@ -7,8 +9,8 @@ def get_asset(asset_contract_address, token_id):
     url = "https://api.opensea.io/api/v1/asset/" + \
         asset_contract_address+"/" + str(token_id)+"/"
 
-    headers = {"X-API-KEY": "24fba988013a492b8e359d6cb2331e0f"}
-    #headers = {"X-API-KEY": "f88ff06861d34122a3d4eb20609fe092"}
+    #headers = {"X-API-KEY": "24fba988013a492b8e359d6cb2331e0f"}
+    headers = {"X-API-KEY": "f88ff06861d34122a3d4eb20609fe092"}
 
     response = requests.request("GET", url, headers=headers)
 
@@ -30,38 +32,55 @@ def main():
 
     # traverse through dapps
     for i in range(len(dapps_df)):
-        #i = 21
+        # specify the location of the dapp in dapps.json
+        i = 9
+
         # dapp_name
         dapp_name = dapps_df.iloc[i]
         dapp_name = dapp_name['slug']
         print(dapp_name)
+        missing_df = pd.read_json(dapp_name + '_missing.json')
+        # print(missing_df.head())
+        # contract_address
+        asset_contract_address = dapps_df.iloc[i, 1]
+        asset_contract_address = asset_contract_address[0]['address']
 
-        # total number of nfts
-        total_nfts = dapps_df.iloc[i, 3]
-        total_nfts = int(total_nfts['total_supply'])
+        # total number of missing nfts
+        missing_list = missing_df[0].tolist()
+        total_nfts = len(missing_list)
+        assets = []
+        count404 = 0
+        error_tokens = []
+        #print(asset_contract_address, total_nfts)
 
-        cleaned_nfts = pd.read_json(dapp_name+'.json')
+        # iterate through missing list and fetch each asset
+        for j in range(total_nfts):
+            token = missing_list[j]
+            response = get_asset(asset_contract_address, token)
+            if(response and response.status_code == 200):
+                assets.append(response.json())
 
-        if len(cleaned_nfts) == total_nfts:
-            print('No missing NFTs for' + dapp_name)
-        else:
-            # get token ids of cleaned nfts
-            existing_df = cleaned_nfts['token_id']
-            existing_df = existing_df.apply(pd.to_numeric)
+            elif (response.status_code == 404):
+                count404 += 1
+            elif (response.status_code == 429):
+                error_tokens.append(token)
 
-            # complete list of tokens
-            complete_tokens = list(range(1, total_nfts))
-            complete_nfts_df = pd.DataFrame(
-                complete_tokens, columns=['token_id'])
+            if count404 != 0 and count404 % 10 == 0:
+                print('sleeping')
+                time.sleep(60)
+                count404 = 0
 
-            # get missing nfts
-            missing_df = complete_nfts_df.merge(
-                existing_df, how='outer', indicator=True).loc[lambda x: x['_merge'] == 'left_only']
-            missing_list = missing_df["token_id"].tolist()
+            if j % 100 == 0 or j == total_nfts-1:
+                # write to file
+                with open('missing' + dapp_name + str(j)+'.json', 'w') as out:
+                    json.dump(assets, out)
+                with open('error' + dapp_name + str(j)+'.json', 'w') as out:
+                    json.dump(error_tokens, out)
+                assets = []
+                error_tokens = []
 
-            # store it to file
-            with open(dapp_name + '_missing.json', 'w') as out:
-                json.dump(missing_list, out)
+            if j % 20 == 0:
+                time.sleep(10)
 
         break
 
