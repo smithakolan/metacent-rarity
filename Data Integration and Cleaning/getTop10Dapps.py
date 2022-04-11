7773d55d5de31638a9eb93e1803a27f58a686e8b
Swaathi Sundaramurugan <ssa388@sfu.ca>
2022-04-10 19:45:34 -0700

    code cleanup

---
+++ Data Integration and Cleaning/getTop10Dapps.py
@@ -0,0 +1,34 @@
+import top10Dapps
+import json
+
+
+def main():
+    with open("dapps.json") as json_file:
+        dapp_list = json.load(json_file)
+
+    selected_dapps = []
+    for dapp in dapp_list:
+        slug = dapp['slug']
+        if slug in top10Dapps.top_10_dapps:
+            print(slug)
+            selected_dapps.append(dapp)
+
+    chosen_dapps = []
+    for sdapp in selected_dapps:
+        sdapp_traits = sdapp['traits']
+        trait_counts = {}
+
+        for key, value in sdapp_traits.items():
+            value_dict = value
+            value_list = value_dict.values()
+            trait_counts[key] = sum(value_list)
+
+        sdapp['trait_count'] = trait_counts
+        chosen_dapps.append(sdapp)
+
+    with open("top_dapps.json", "w") as f:
+        json.dump(chosen_dapps, f)
+
+
+if __name__ == '__main__':
+    main()
